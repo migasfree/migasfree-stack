33e7aa415c0240bacc79ad5da7976891576787fb
Alberto Gacías <alberto@migasfree.org>
2021-03-27 10:15:30 +0100

    Default settings.py

---
+++ config/conf/settings.py
@@ -0,0 +1,121 @@
+import os
+
+def get_secret_pass():
+    password = ""
+    with open("/run/secrets/password_database","r") as f:
+        password = f.read()
+    return password
+
+DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': os. environ['POSTGRES_DB'],
+            'USER': os. environ['POSTGRES_USER'],
+            'PASSWORD': get_secret_pass(),
+            'HOST': os. environ['POSTGRES_HOST'],
+            'PORT': os. environ['POSTGRES_PORT'],
+        }
+    }
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'verbose': {
+            'format': '%(levelname)s %(asctime)s %(module)s %(process)d %(thread)d %(message)s'
+        },
+        'simple': {
+            'format': '%(levelname)s %(message)s'
+        },
+    },
+    'handlers': {
+        'file': {
+            'level': 'DEBUG',
+            'class': 'logging.FileHandler',
+            'filename': '/tmp/migasfree.log',
+            'formatter': 'verbose',
+        },
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+            'formatter': 'verbose',
+        },
+    },
+    'loggers': {
+        'migasfree': {
+            'handlers': ['console', 'file'],
+            'level': 'DEBUG',
+        }
+    },
+    'root': {
+        'level': 'INFO',
+        'handlers': ['console', 'file']
+    },
+}
+
+
+
+# DATASTORE
+# =========
+REDIS_HOST = 'datastore'
+REDIS_PORT = 6379
+REDIS_DB = 0
+BROKER_URL = 'redis://%s:%d/%d' % (REDIS_HOST, REDIS_PORT, REDIS_DB)
+CACHES = {
+    "default": {
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": BROKER_URL,
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient",
+        }
+    }
+}
+
+
+
+# NECESSARY FOR SWAGGER AND REST-FRAMEWORK
+# Setup support for proxy headers
+#SWAGGER_SETTINGS['DEFAULT_INFO']='import.path.to.urls.api_info'
+USE_X_FORWARDED_HOST = True
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+
+DEBUG = False
+
+
+# Configuracion del correo
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'webmail.mydomain.es'
+EMAIL_PORT = 25
+EMAIL_HOST_USER = "myuser"
+EMAIL_HOST_PASSWORD = ""
+DEFAULT_FROM_EMAIL = "migasfree-server <noreply@mydomain.es>"
+ADMINS = [('mymame', 'myuser@mydomain.es'),]
+
+#MIGASFREE_PUBLIC_DIR = os.path.join(MIGASFREE_PROJECT_DIR,'public')
+#STATIC_ROOT = '/var/lib/migasfree-backend/static'
+#MEDIA_ROOT = MIGASFREE_PUBLIC_DIR
+
+MEDIA_URL ="/public/"
+MIGASFREE_TMP_DIR = '/var/tmp'
+
+MIGASFREE_EXTERNAL_ACTIONS = {
+        "computer": {
+            "ping": {"title": "PING", "description": "check connectivity"},
+            "ssh": {"title": "SSH", "description": "remote control via ssh"},
+            "vnc": {"title": "VNC", "description": "remote control vnc", "many": False},
+            "sync": {"title": "SYNC", "description": "ssh -> run migasfree -u"},
+            "install": {
+                "title": "INSTALL",
+                "description": "ssh -> install a package",
+                "related": ["deployment", "computer"]
+            },
+        },
+        "error": {
+            "clean": {"title": "delete", "description": "delete errors"},
+        }
+}
+
+MIGASFREE_ORGANIZATION="ACME"
+MIGASFREE_HELP_DESK="Help Desk: 555 555 555"
+#MIGASFREE_COMPUTER_SEARCH_FIELDS=('name','id','ip_address','forwarded_ip_address')
--- run/nfs-server.sh
+++ run/nfs-server.sh
@@ -10,11 +10,17 @@ mkdir -p /exports/migasfree/keys
 
 chown 70:70 /exports/migasfree/database
 chown 70:70 /exports/migasfree/dump
-chown 1000:1000 /exports/migasfree/datastore
+chown 999:999 /exports/migasfree/datastore
 chown 890:890  /exports/migasfree/conf
 chown 890:890  /exports/migasfree/public
 chown 890:890  /exports/migasfree/keys
 
+
+if ! [ -f /exports/migasfree/conf/settings.py ];
+then
+    cp ../config/conf/settings.py  /exports/migasfree/conf/
+    chown 890:890  /exports/migasfree/conf/settings
+fi
 
 
 docker run --restart=always -d \
