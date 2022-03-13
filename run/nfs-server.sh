40c76896d3535e6eae85e80ed7053e7170191c33
Alberto Gacías <alberto@migasfree.org>
2022-03-13 09:33:53 +0100

    Modified run

--- run/nfs-server.sh
+++ run/nfs-server.sh
@@ -1,22 +1,27 @@
 #https://github.com/sjiveson/nfs-server-alpine
 
-. ../config/env/globals
+source ../config/env/general
+#source ../config/env/stack
 
 mkdir -p /exports/migasfree/database
 mkdir -p /exports/migasfree/dump
 mkdir -p /exports/migasfree/datastore
-mkdir -p /exports/migasfree/conf
+mkdir -p /exports/migasfree/conf/locations.d
 mkdir -p /exports/migasfree/public
 mkdir -p /exports/migasfree/keys
+mkdir -p /exports/migasfree/tmp
+mkdir -p /exports/migasfree/plugins
 mkdir -p /exports/migasfree/certificates
 
 
 chown 70:70 /exports/migasfree/database
 chown 70:70 /exports/migasfree/dump
 chown 999:999 /exports/migasfree/datastore
-chown 890:890  /exports/migasfree/conf
+chown -R 890:890  /exports/migasfree/conf
 chown 890:890  /exports/migasfree/public
 chown 890:890  /exports/migasfree/keys
+chown 890:890  /exports/migasfree/tmp
+chown 890:890 /exports/migasfree/plugins
 chown root:root /exports/migasfree/certificates
 
 
@@ -25,6 +30,14 @@ then
     cp ../config/conf/settings.py  /exports/migasfree/conf/
     chown 890:890  /exports/migasfree/conf/settings
 fi
+
+if ! [ -f /exports/migasfree/plugins/__init__.py ]
+then
+    touch /exports/migasfree/plugins/__init__.py
+    chown 890:890 /exports/migasfree/plugins/__init__.py
+fi
+
+
 
 _DEV=$(ip -4 route ls | grep default | grep -Po '(?<=dev )(\S+)')
 ip addr add ${NFS_SERVER}/24 dev $_DEV
--- run/run.sh
+++ run/run.sh
@@ -1,27 +1,5 @@
-. ../config/env/globals
-
-export FQDN
-
-export NFS_SERVER
-
-export STACK
-
-export CERTBOT_TAG=$(cat ../build/loadbalancer/VERSION)
-
-export LOADBALANCER_TAG=$(cat ../build/loadbalancer/VERSION)
-
-export BACKEND_TAG=$(cat ../build/backend/VERSION)
-
-export FRONTEND_TAG=$(cat ../build/frontend/VERSION)
-
-export PUBLIC_TAG=$(cat ../build/public/VERSION)
-
-export PMS_APT_TAG=$(cat ../build/pms-apt/VERSION)
-
-export DATABASE_TAG=$(cat ../build/database/VERSION)
-
-export DATASTORE_TAG=$(cat ../build/datastore/VERSION)
-
+#source ../config/env/general
+source ../config/env/stack
 
 # NETWORK
 docker network ls | grep ${STACK}_network
@@ -30,6 +8,9 @@ then
     docker network create --driver=overlay --attachable ${STACK}_network
 fi
 
+
+# Generates core.yml  & migasfree.yml from templates
+./templates/process
 
 docker stack deploy --compose-file core.yml core
 
