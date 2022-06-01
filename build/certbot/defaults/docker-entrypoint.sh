2e55a35ba25112a0288bbda10bebaef907719063
Alberto Gacías <alberto@migasfree.org>
2022-06-01 17:25:19 +0200

    upgraded docker-entrypoint.sh file in certbot

--- build/certbot/defaults/docker-entrypoint.sh
+++ build/certbot/defaults/docker-entrypoint.sh
@@ -1,8 +1,19 @@
 #!/bin/sh
 trap exit TERM
 
+function send_message {
+    point="http://loadbalancer:8001/services/message"
+    data="{ \"text\":\"$1\", \"service\":\"$SERVICE\" ,\"node\":\"$NODE\",\"container\":\"$HOSTNAME\" }"
+    until [ $(curl -s -o /dev/null  -w '%{http_code}' -d "$data" -H "Content-Type: application/json" -X POST $point) = "200" ]
+    do
+       sleep 2
+    done
+}
+
 while :;
 do
+    send_message "renew certificate letsencript" 
     [ "${HTTPSMODE}" = "auto" ] && . /usr/bin/renew-certificates.sh
+    send_message ""
     sleep 12h & wait ${!}
 done
