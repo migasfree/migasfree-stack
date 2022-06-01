21e219536836c7972d67eccacf94a56f351332e5
Alberto Gacías <alberto@migasfree.org>
2022-06-01 17:33:23 +0200

    upgraded docker-entrypoint.sh file in pms-apt

--- build/pms-apt/defaults/docker-entrypoint.sh
+++ build/pms-apt/defaults/docker-entrypoint.sh
@@ -25,9 +25,21 @@ function wait {
     exit
 }
 
+
+function send_message {
+    point="http://loadbalancer:8001/services/message"
+    data="{ \"text\":\"$1\", \"service\":\"$SERVICE\" ,\"node\":\"$NODE\",\"container\":\"$HOSTNAME\" }"
+    until [ $(curl -s -o /dev/null  -w '%{http_code}' -d "$data" -H "Content-Type: application/json" -X POST $point) = "200" ]
+    do
+       sleep 2
+    done
+}
+
+
 function reload_loadbalancer {
     curl -d "" -X POST http://loadbalancer:8001/services/reconfigure &> /dev/null 
 }
+
 
 # ENVIRONMENT VARIABLES FOR VOLUMES  
 function get_mount_paths {
@@ -43,6 +55,10 @@ function get_mount_paths {
     IFS=""
 }
 
+
+send_message "starting ${SERVICE:(${#STACK})+1}"
+
+send_message "waiting backend"
 wait backend 8080
 
 get_mount_paths 
@@ -65,5 +81,7 @@ echo "
 
 cd /pms
 reload_loadbalancer
+
+send_message ""
 celery -A migasfree.core.tasks -b $BROKER_URL --result-backend=$BROKER_URL  worker -l INFO --uid=890 -Q $QUEUES --concurrency=1
 
