9c35a309a7de4d04eb33fa8a79f219ac22d78576
Alberto Gacías <alberto@migasfree.org>
2022-06-01 17:22:13 +0200

    upgraded save.sh file

--- images/save.sh
+++ images/save.sh
@@ -1,7 +1,7 @@
 
 
 _VERSION=5.0
-for _IMG in loadbalancer pms-apt public database datastore client certbot
+for _IMG in loadbalancer public database datastore client certbot pms-apt  pms-yum pms-winget pms-pacman
 do
     docker save --output ./migasfree-${_IMG}-${_VERSION}.tar migasfree/${_IMG}:${_VERSION}
 done
