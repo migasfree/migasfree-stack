1e155ef219f5b900b04d00ab36dd3aee5d77e466
Jose Antonio Chavarría <jachavar@gmail.com>
2022-06-08 13:26:28 +0200

    Added _SERVER constant

--- build/pms-yum/run.sh
+++ build/pms-yum/run.sh
@@ -1,22 +1,17 @@
+_SERVER="192.168.92.100"
 
-
-mount |grep "^192.168.92.100:/export/migasfree" >/dev/null
-if ! [ $? = 0 ]
+mount |grep "^$_SERVER:/export/migasfree" >/dev/null
+if [ $? -ne 0 ]
 then
-    mount -t nfs 192.168.92.100:/export/migasfree /var/lib/migasfree
+    mount -t nfs $_SERVER:/export/migasfree /var/lib/migasfree
 fi
 
-
-docker run --rm -ti  \
-  -v "/var/lib/migasfree/192.168.92.100/public:/var/migasfree/repo" \
-  -v "/var/lib/migasfree/192.168.92.100/keys:/usr/share/migasfree-server" \
+docker run --rm -ti \
+  -v "/var/lib/migasfree/$_SERVER/public:/var/migasfree/repo" \
+  -v "/var/lib/migasfree/$_SERVER/keys:/usr/share/migasfree-server" \
   migasfree/apt:latest \
   su -c "/app/repository-create 1" - www-data
 
-
-
-
-
-mkdir /var/migasfree;chown 890:890 /var/migasfree
+mkdir /var/migasfree
+chown 890:890 /var/migasfree
 su -c "/app/repository-create 1" www-data
-
