d446fd3c1ed7389b21c592782c6836c9974cb533
Jose Antonio Chavarría <jachavar@gmail.com>
2022-06-08 13:08:00 +0200

    Fixed indentation

--- build/frontend/defaults/docker-entrypoint.sh
+++ build/frontend/defaults/docker-entrypoint.sh
@@ -7,30 +7,28 @@ function wait {
     until [ $counter -gt 5 ]
     do
         nc -z $_SERVER $_PORT 2> /dev/null
-       if  [ $? = 0 ]
-       then
-           echo "$_SERVER:$_PORT is running."
-           return
-       else
-           echo "$_SERVER:$_PORT is not running after $counter seconds."
-           sleep 1
-       fi
+        if [ $? = 0 ]
+        then
+            echo "$_SERVER:$_PORT is running."
+            return
+        else
+            echo "$_SERVER:$_PORT is not running after $counter seconds."
+            sleep 1
+        fi
         ((counter++))
     done
     echo "Rebooting container"
     exit
 }
 
-
 function send_message {
     point="http://loadbalancer:8001/services/message"
     data="{ \"text\":\"$1\", \"service\":\"$SERVICE\" ,\"node\":\"$NODE\",\"container\":\"$HOSTNAME\" }"
     until [ $(curl -s -o /dev/null  -w '%{http_code}' -d "$data" -H "Content-Type: application/json" -X POST $point) = "200" ]
     do
-       sleep 2
+        sleep 2
     done
 }
-
 
 function reload_loadbalancer {
     curl -d "" -X POST http://loadbalancer:8001/services/reconfigure &> /dev/null
@@ -41,10 +39,10 @@ cat << EOF > /etc/nginx/conf.d/default.conf
 server {
     listen       80;
     server_name  localhost 127.0.0.1 frontend $(hostname);
-    
+
     #charset koi8-r;
     #access_log  /var/log/nginx/host.access.log  main;
-    
+
     access_log  /dev/stdout  main;
     error_log /dev/stderr warn;
 
@@ -67,10 +65,8 @@ server {
     location = /50x.html {
         root   /usr/share/nginx/html;
     }
-
 }
 EOF
-
 
 send_message "starting ${SERVICE:(${#STACK})+1}"
 
@@ -78,9 +74,8 @@ send_message "starting ${SERVICE:(${#STACK})+1}"
 _FILES=$(grep -l __FQDN__ /usr/share/nginx/html/js/*)
 for _FILE in $_FILES
 do
-    sed -i "s/__FQDN__/$FQDN/g" $_FILE 
+    sed -i "s/__FQDN__/$FQDN/g" $_FILE
 done
-
 
 send_message "waiting backend"
 wait backend 8080
@@ -104,4 +99,4 @@ echo "
 echo "daemon off;" >> /etc/nginx/nginx.conf
 reload_loadbalancer
 send_message ""
-nginx 
+nginx
