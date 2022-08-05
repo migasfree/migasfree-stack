ae76e691c808ae4dc38e1f3d256a59b0105d0903
Jose Antonio Chavarría <jachavar@gmail.com>
2022-08-05 12:10:10 +0200

    Cleanup

--- run/scale.sh
+++ run/scale.sh
@@ -1,12 +1,12 @@
+#!/bin/bash
+
 _SERVICE=$1
 _NUM=$2
 
 docker service scale ${_SERVICE}=${_NUM}
 
-
 _CMD="echo '@master reload' | socat /var/run/haproxy-master-socket stdio"
 
-
-_BALANCER=$(docker ps |grep loadbalancer|awk '{print $1}')
+_BALANCER=$(docker ps | grep loadbalancer | awk '{print $1}')
 
 docker exec $_BALANCER reconfigure
