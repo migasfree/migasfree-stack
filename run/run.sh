eedb0669de8ce3139fb88d6aa99447801d5cbbb7
Jose Antonio Chavarría <jachavar@gmail.com>
2022-08-05 12:01:53 +0200

    Fixed expression (more readable)

--- run/run.sh
+++ run/run.sh
@@ -3,31 +3,29 @@ source ../config/env/stack
 
 # NETWORK
 docker network ls | grep ${STACK}_network
-if ! [ $? = 0 ]
+if [ $? -ne 0 ]
 then
     docker network create --driver=overlay --attachable ${STACK}_network
 fi
 
-
-# Generates core.yml  & migasfree.yml from templates
+# Generates core.yml & migasfree.yml from templates
 ./templates/process
 
 docker stack deploy --compose-file core.yml core
-echo -n "waiting core " 
-while true 
+echo -n "waiting core "
+while true
 do
     _STATUS=$(curl --write-out '%{http_code}' --silent --output /dev/null ${FQDN}/services/status)
     if [ "${_STATUS}" = "200" ]
     then
-
-echo "
+        echo "
 
 
                    ●                          ●●
                                              ●
-         ●●● ●●    ●    ●●     ●●●     ●●●  ●●●●  ●●●  ●●●    ●●● 
+         ●●● ●●    ●    ●●     ●●●     ●●●  ●●●●  ●●●  ●●●    ●●●
         ●   ●  ●   ●   ●  ●       ●   ●      ●   ●    ●   ●  ●   ●
-        ●   ●  ●   ●   ●  ●    ●●●●    ●●    ●   ●    ●●●●   ●●●● 
+        ●   ●  ●   ●   ●  ●    ●●●●    ●●    ●   ●    ●●●●   ●●●●
         ●   ●  ●   ●   ●  ●   ●   ●      ●   ●   ●    ●      ●
         ●   ●  ●   ●    ●●●    ●●●    ●●●    ●   ●     ●●●    ●●●
                           ●
