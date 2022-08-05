7089bb94eff8ca3a368d02f2580fe45722ad4f25
Jose Antonio Chavarría <jachavar@gmail.com>
2022-08-05 12:09:53 +0200

    Fixed docker system prune command

--- run/stop.sh
+++ run/stop.sh
@@ -1,26 +1,22 @@
 #/bin/bash
 
-
 docker stack rm mf
-while docker ps|grep "mf_" > /dev/null
-do 
-   echo -n "."
-   sleep 1
+while docker ps | grep "mf_" > /dev/null
+do
+    echo -n "."
+    sleep 1
 done
 echo
- 
 
 docker stack rm core
-while docker ps|grep "core_" > /dev/null
-do 
-   echo -n "."
-   sleep 1
+while docker ps | grep "core_" > /dev/null
+do
+    echo -n "."
+    sleep 1
 done
-echo 
+echo
 
 docker stop nfsserver
 docker rm nfsserver
 
-
-
-echo "y" | docker system prune
+docker system prune -f
