ba4f3ba7d6fed8e1f23856253cbe63bbd3f6ff72
Jose Antonio Chavarría <jachavar@gmail.com>
2022-08-05 12:52:48 +0200

    Cleanup

--- build/build.sh
+++ build/build.sh
@@ -1,24 +1,25 @@
+#!/bin/bash
+
 function build
 {
-    CONTEXT="$1"
-    cd $CONTEXT
-    TAG=$(cat VERSION)
+    local _CONTEXT="$1"
+    pushd "$_CONTEXT" > /dev/null
+    local _TAG=$(cat VERSION)
     echo
     echo
-    echo "BUILD: ${IMAGE}:${TAG}"
+    echo "BUILD: ${_CONTEXT}:${_TAG}"
     echo "============================================================================"
-    docker build . -t "migasfree/$CONTEXT:$TAG"
-    cd - >/dev/null
+    docker build . -t "migasfree/${_CONTEXT}:${_TAG}"
+    popd > /dev/null
 }
 
-
-IMAGES="$1"
-if [ -z "${IMAGES}" ]
+_IMAGES="$1"
+if [ -z "${_IMAGES}" ]
 then
-   IMAGES="loadbalancer certbot datastore database backend frontend public pms-apt pms-yum pms-winget pms-pacman"
+    _IMAGES="loadbalancer certbot datastore database backend frontend public pms-apt pms-yum pms-winget pms-pacman"
 fi
 
-for IMAGE in $IMAGES
+for _IMAGE in ${_IMAGES}
 do
-   build $IMAGE
+    build "${_IMAGE}"
 done
