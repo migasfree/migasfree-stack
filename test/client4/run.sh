60c346dc59da115da08db894af7f3840b37c24e4
Jose Antonio Chavarría <jachavar@gmail.com>
2022-08-18 09:25:33 +0200

    Cleanup

--- test/client4/run.sh
+++ test/client4/run.sh
@@ -3,17 +3,17 @@
 source ../../config/env/general
 source ../../config/env/stack
 
-
 if [ "$HTTPSMODE" = "manual" ]
 then
     cp /exports/migasfree/certificates/ca.crt defaults/usr/share/ca-certificates/ca.crt
 fi
+
 docker build . -t migasfree/client:4.20
 docker run --rm \
-	-e TZ="Europe/Madrid" \
-	-e MIGASFREE_CLIENT_SERVER=${FQDN}:443 \
-	-e MIGASFREE_CLIENT_PROJECT=acme \
+    -e TZ="Europe/Madrid" \
+    -e MIGASFREE_CLIENT_SERVER=${FQDN}:443 \
+    -e MIGASFREE_CLIENT_PROJECT=acme \
     -e MIGASFREE_CLIENT_PROTOCOL=https \
-	-e MIGASFREE_CLIENT_PORT= \
+    -e MIGASFREE_CLIENT_PORT= \
     -e USER=root \
-	-ti migasfree/client:4.20 bash 
+    -ti migasfree/client:4.20 bash
