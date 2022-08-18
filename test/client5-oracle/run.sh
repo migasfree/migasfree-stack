b0bb93168053b0c3fe4280460d75bd646e3d1fc1
Jose Antonio Chavarría <jachavar@gmail.com>
2022-08-18 09:24:16 +0200

    Cleanup

--- test/client5-oracle/run.sh
+++ test/client5-oracle/run.sh
@@ -6,15 +6,16 @@ source ../../config/env/stack
 if [ "$HTTPSMODE" = "manual" ]
 then
     cp /exports/migasfree/certificates/ca.crt defaults/usr/share/ca-certificates/ca.crt
-    # cert mus be in format PEM
+    # cert must be in PEM format
     openssl x509 -in defaults/usr/share/ca-certificates/ca.crt -out defaults/usr/share/ca-certificates/ca.pem -outform PEM
 fi
+
 docker build . -t migasfree/client-oracle:5.0
 docker run --rm \
-	-e TZ="Europe/Madrid" \
-	-e MIGASFREE_CLIENT_SERVER=${FQDN} \
-	-e MIGASFREE_CLIENT_PROJECT=fedora \
-        -e MIGASFREE_CLIENT_PROTOCOL=https \
-	-e MIGASFREE_CLIENT_PORT= \
-        -e USER=root \
-	-ti migasfree/client-oracle:5.0 bash 
+    -e TZ="Europe/Madrid" \
+    -e MIGASFREE_CLIENT_SERVER=${FQDN} \
+    -e MIGASFREE_CLIENT_PROJECT=fedora \
+    -e MIGASFREE_CLIENT_PROTOCOL=https \
+    -e MIGASFREE_CLIENT_PORT= \
+    -e USER=root \
+    -ti migasfree/client-oracle:5.0 bash
--- test/client5/run.sh
+++ test/client5/run.sh
@@ -7,12 +7,13 @@ if [ "$HTTPSMODE" = "manual" ]
 then
     cp /exports/migasfree/certificates/ca.crt defaults/usr/share/ca-certificates/ca.crt
 fi
+
 docker build . -t migasfree/client:5.0
 docker run --rm \
-	-e TZ="Europe/Madrid" \
-	-e MIGASFREE_CLIENT_SERVER=${FQDN} \
-	-e MIGASFREE_CLIENT_PROJECT=acme \
+    -e TZ="Europe/Madrid" \
+    -e MIGASFREE_CLIENT_SERVER=${FQDN} \
+    -e MIGASFREE_CLIENT_PROJECT=acme \
     -e MIGASFREE_CLIENT_PROTOCOL=https \
-	-e MIGASFREE_CLIENT_PORT= \
+    -e MIGASFREE_CLIENT_PORT= \
     -e USER=root \
-	-ti migasfree/client:5.0 bash 
+    -ti migasfree/client:5.0 bash
